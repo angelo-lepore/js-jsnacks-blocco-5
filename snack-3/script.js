532e67201a38afa715e79ee4cfc1fb64e6ed3ec2
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-13 14:39:30 +0200

    finished-snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+console.log(numbers);
+
+const numbers_plus_one = numbers.map((number) => number + 1);
+
+console.log(numbers_plus_one);
