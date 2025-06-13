61d8796b5a51e20c6cd3c44e16ae386fe5811b87
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-13 14:11:02 +0200

    finished-snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,10 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log(names);
+
+names.forEach((name) => {
+  console.log(name);
+});
