992cd50fbbb7b9368930c84b61456f8e44fee4e6
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-13 15:22:12 +0200

    finished-snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,35 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const student = "Marco Lanci";
+
+let class_student = null;
+
+students.forEach((element) => {
+  if (element.name === student) {
+    class_student = element.class;
+  }
+});
+
+console.log(class_student);
